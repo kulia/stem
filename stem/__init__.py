414615e41a5c343b967a69c56e667fbd945118e5
Geir Kulia <geir.kulia@signalanalysislab.com>
2019-03-31 04:26:32 +0200

    make stem module

---
+++ stem/__init__.py
@@ -0,0 +1,4 @@
+from stem.stem import stem
+
+from sys import modules
+modules[__name__] = stem
--- stem/stem.py
+++ stem/stem.py
@@ -17,6 +17,10 @@ def stem(*args, **kwargs):
         Identical to matplotlib.pyplot.plot(**kwargs).
     """
     import matplotlib.pyplot as plt
+    from copy import copy
+
+    if len(args) == 0:
+        return {}
 
     # Remove string styling for ploting
     if type(args[-1]) is type(' '):
@@ -36,22 +40,35 @@ def stem(*args, **kwargs):
     # Set color to next in cycle if not defined
     kwargs['c'] = kwargs.pop('c', next(ax._get_lines.prop_cycler)['color'])
 
+    # Set label to nothing if not defined
+    kwargs['label'] = kwargs.pop('label', '')
+
     # Set baseline to min of y if not defined
     baseline = kwargs.pop('baseline', min(y))
 
     # Plot baseline
-    ax.plot([min(x), max(x)], [baseline, baseline], **kwargs)
+    kwargs_bl = copy(kwargs)
+    del kwargs_bl['label']
+    ax.plot([min(x), max(x)], [baseline, baseline], **kwargs_bl)
 
     # Plot stem line from baseline to marker
     for xi, yi in zip(x, y):
-        ax.plot([xi, xi], [baseline, yi], **kwargs)
+        kwargs_l = copy(kwargs)
+        del kwargs_l['label']
+        ax.plot([xi, xi], [baseline, yi], **kwargs_l)
 
     # Plot stem marker
-    from copy import copy
     kwargs_o = copy(kwargs)
     kwargs_o['marker'] = 'o'
     kwargs_o['linestyle'] = ''
     ax.plot(x, y, **kwargs_o)
+
+    '''
+    TODO: Fix Stem handles.  Hint:
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels)
+    https://matplotlib.org/users/legend_guide.html
+    '''
 
 from sys import modules
 modules[__name__] = stem
